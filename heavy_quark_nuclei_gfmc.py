90539c7ec69d34fd40fbc2f6d1f20e536763a962
Michael Wagman <mwagman@wc.fnal.gov>
2022-09-06 13:40:07 -0500

    CF

--- heavy_quark_nuclei_gfmc.py
+++ heavy_quark_nuclei_gfmc.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 import scipy.interpolate
+import scipy.special
 import pickle
 import paper_plt
 import tqdm.auto as tqdm
@@ -20,6 +21,7 @@ from itertools import repeat
 import time
 import h5py
 import math
+import mpmath
 
 from sympy import simplify, summation, sqrt, Eq, Integral, oo, pprint, symbols, Symbol, log, exp, diff, Sum, factorial, IndexedBase, Function, cos, sin, atan, acot, pi, atan2, trigsimp, lambdify, re, im
 from sympy.physics.hydrogen import R_nl, Psi_nlm
@@ -39,18 +41,22 @@ parser.add_argument('--resampling', type=int, default=None)
 parser.add_argument('--alpha', type=float, default=1)
 parser.add_argument('--Nc', type=int, default=2)
 parser.add_argument('--N_coord', type=int, default=2)
-parser.add_argument('--a0', type=float, default=2)
+parser.add_argument('--nf', type=int, default=5)
+parser.add_argument('--OLO', type=str, default="LO")
+parser.add_argument('--spoila', type=int, default=1)
+parser.add_argument('--spoilf', type=str, default="hwf")
 parser.add_argument('--outdir', type=str, required=True)
 globals().update(vars(parser.parse_args()))
 
-cF = (Nc**2 - 1)/(2*Nc)
-VB = alpha*cF
-if N_coord > 2:
-    VB = alpha*cF/(Nc-1)
-
 #######################################################################################
+
+CF = (Nc**2 - 1)/(2*Nc)
+VB = alpha*CF
+if N_coord > 2:
+    VB = alpha*CF/(Nc-1)
+
 cutoff = 1;
-a0 = 2/VB;
+a0 = spoila*2/VB;
 
 rr = np.full((N_coord,N_coord), fill_value = '',dtype=object)
 for i in range(N_coord):
@@ -148,10 +154,16 @@ def Chi_no_v_test(N_coord, r, t, p, C, A):
 
 def Chi_no_v(N_coord, r, t, p, C, A):
     Chi = 1;
-    for i in range(N_coord):
-        for j in range(N_coord):
-            if i!=j and j>=i:
-                Chi = Chi*exp(-rrSpher(i,j,r,t,p)/A[0])
+    if spoilf == "hwf":
+    	for i in range(N_coord):
+       		for j in range(N_coord):
+            		if i!=j and j>=i:
+                		Chi = Chi*exp(-rrSpher(i,j,r,t,p)/A[0])
+    elif spoilf == "gauss":
+    	for i in range(N_coord):
+        	for j in range(N_coord):
+            		if i!=j and j>=i:
+                		Chi = Chi*exp(-1/2*(rrSpher(i,j,r,t,p)/A[0])**2)
     return C[0]*Chi
 
 print(simplify(Chi_no_v_test(N_coord, r, t, p, C, A)))
@@ -298,8 +310,7 @@ def metropolis_coordinate_ensemble(this_psi, *, n_therm, N_walkers, n_skip, eps)
     # metropolis updates
     print("Running Metropolis")
     #for i in tqdm.tqdm(range(-n_therm, N_walkers*n_skip)):
-    for i in range(-n_therm, N_walkers*n_skip):
-        # update
+    for i in range(-n_therm, N_walkers*n_skip): # update
         dR = draw_coordinates(R.shape, eps=eps, axis=1)
         new_R = R + dR
         # accept/reject based on |psi(R)|^2
@@ -317,9 +328,9 @@ def metropolis_coordinate_ensemble(this_psi, *, n_therm, N_walkers, n_skip, eps)
             Rs[this_walker,:,:] = R
             psi2s[this_walker] = p_R
             this_walker += 1
-            #print(f'iteration {i+1}')
-            #print(f'|psi(R)|^2 = {p_R}')
-            #print(f'Total acc frac = {acc / (i+1)}')
+            print(f'iteration {i+1}')
+            print(f'|psi(R)|^2 = {p_R}')
+            print(f'acc frac = {acc / (i+1)} \n')
     print(f'Total acc frac = {acc / (i+1)}')
     # return coordinates R and respective |psi(R)|^2
     return Rs, psi2s
@@ -374,9 +385,45 @@ class wvfn(nn.Module):
 tau_iMev = dtau_iMev * n_step
 xs = np.linspace(0, tau_iMev, endpoint=True, num=n_step+1)
 
+beta0 = 11/3*Nc - 2/3*nf
+beta1 = 34/3*Nc**2 - 20/3*Nc*nf/2 - 2*CF*nf
+beta2 = 2857/54*Nc**3 + CF**2*nf-205/9*Nc*CF*nf/2-1415/27*Nc**2*nf/2+44/9*CF*(nf/2)**2+158/27*Nc*(nf/2)**2
+aa1 = 31/9*Nc-10/9*nf
+zeta3 = scipy.special.zeta(3) 
+zeta5 = scipy.special.zeta(5)
+zeta51 = 1/2 + 1/3 + 1/7 + 1/51 + 1/4284
+zeta6 = scipy.special.zeta(6)
+aa2 = ( 4343/162 + 6*np.pi**2 - np.pi**4/4 + 22/3*zeta3 )*Nc**2 - ( 1798/81 + 56/3*zeta3 )*Nc*nf/2 - ( 55/3 - 16*zeta3  )*CF*nf/2 + (10/9*nf)**2
+dFF = (18-Nc**2+Nc**4)/(96*Nc**2)
+dFA = Nc*(Nc**2+6)/48
+alpha4 = float(mpmath.polylog(4,1/2))*0+(-jax.numpy.log(2))**4/(4*3*2*1)
+ss6 = zeta51+zeta6
+aa30 = dFA*( np.pi**2*( 7432/9-4736*alpha4+jax.numpy.log(2)*(14752/3-3472*zeta3)-6616*zeta3/3)  +  np.pi**4*(-156+560*jax.numpy.log(2)/3+496*jax.numpy.log(2)**2/3)+1511*np.pi**6/45)  + Nc**3*(385645/2916 + np.pi**2*( -953/54 +584/3*alpha4 +175/2*zeta3 + jax.numpy.log(2)*(-922/9+217*zeta3/3) ) +584*zeta3/3 + np.pi**4*( 1349/270-20*jax.numpy.log(2)/9-40*jax.numpy.log(2)**2/9 ) -1927/6*zeta5 -143/2*zeta3**2-4621/3024*np.pi**6+144*ss6  ) 
+aa31 = dFF*( np.pi**2*(1264/9-976*zeta3/3+jax.numpy.log(2)*(64+672*zeta3)) + np.pi**4*(-184/3+32/3*jax.numpy.log(2)-32*jax.numpy.log(2)**2) +10/3*np.pi**6 ) + CF**2/2*(286/9+296/3*zeta3-160*zeta5)+Nc*CF/2*(-71281/162+264*zeta3+80*zeta5)+Nc**2/2*(-58747/486+np.pi**2*(17/27-32*alpha4+jax.numpy.log(2)*(-4/3-14*zeta3)-19/3*zeta3)-356*zeta3+np.pi**4*(-157/54-5*jax.numpy.log(2)/9+jax.numpy.log(2)**2)+1091*zeta5/6+57/2*zeta3**2+761*np.pi**6/2520-48*ss6)
+aa32 = Nc/4*(12541/243+368/3*zeta3+64*np.pi**4/135)+CF/4*(14002/81-416*zeta3/3)
+aa33 = -(20/9)**3*1/8
+aa3 = aa30+aa31*nf+aa32*nf**2+aa33*nf**3
+Rprime = lambda R: adl.norm_3vec(R)*jax.numpy.exp(np.euler_gamma)
 # build Coulomb potential
 AV_Coulomb = {}
-AV_Coulomb['O1'] = lambda R: -1*VB/adl.norm_3vec(R)
+if OLO == "LO":
+	AV_Coulomb['O1'] = lambda R: -1*VB/adl.norm_3vec(R)
+elif OLO == "NLO":
+	AV_Coulomb['O1'] = lambda R: -1*VB/adl.norm_3vec(R)*(1 + alpha_s/(4*np.pi)*(2*beta0*jax.numpy.log(Rprime(R))+aa1))
+elif OLO == "NNLO":
+	AV_Coulomb['O1'] = lambda R: -1*VB/adl.norm_3vec(R)*(1 + alpha_s/(4*np.pi)*(2*beta0*jax.numpy.log(Rprime(R))+aa1) + (alpha_s/(4*np.pi))**2*( beta0**2*(4*jax.numpy.log(Rprime(R))**2 + np.pi**2/3) + 2*( beta1+2*beta0*aa1 )*jax.numpy.log(Rprime(R))+aa2 ) )
+elif OLO == "N3LO":
+        AV_Coulomb['O1'] = lambda R: -1*VB/adl.norm_3vec(R)*(1 + alpha_s/(4*np.pi)*(2*beta0*jax.numpy.log(Rprime(R))+aa1) + (alpha_s/(4*np.pi))**2*( beta0**2*(4*jax.numpy.log(Rprime(R))**2 + np.pi**2/3) + 2*( beta1+2*beta0*aa1 )*jax.numpy.log(Rprime(R))+aa2 ) ) + (alpha_s/(4*np.pi))**3*( 64*np.pi**2/3*Nc**3*jax.numpy.log(adl.norm_3vec(R)) + aa3 + 64*np.pi**2/3*Nc**3*np.euler_gamma + 512*beta0**3*( jax.numpy.log(Rprime(R))**3 + np.pi**4/4*jax.numpy.log(Rprime(R))+2*zeta3 ) + (640*beta0*beta1 + 192*beta0**2*aa1)*(jax.numpy.log(Rprime(R))**2+np.pi**2/12) + (128*beta2+64*beta1*aa1+24*beta0*aa2)*jax.numpy.log(Rprime(R)) ) 
+elif OLO == "mNLO":
+        AV_Coulomb['O1'] = lambda R: -1*VB/adl.norm_3vec(R)*(1 + alpha_s/(4*np.pi)*(2*beta0*jax.numpy.log(Rprime(R))+aa1)) -1*CF*Nc*alpha_s**2/(N_coord-1)/(adl.norm_3vec(R)**2)
+elif OLO == "mNNLO": 
+	AV_Coulomb['O1'] = lambda R: -1*VB/adl.norm_3vec(R)*(1 + alpha_s/(4*np.pi)*(2*beta0*jax.numpy.log(Rprime(R))+aa1) + (alpha_s/(4*np.pi))**2*( beta0**2*(4*jax.numpy.log(Rprime(R))**2 + np.pi**2/3) + 2*( beta1+2*beta0*aa1 )*jax.numpy.log(Rprime(R))+aa2 ) ) -1*CF*Nc*alpha_s**2/(N_coord-1)/(adl.norm_3vec(R)**2)
+elif OLO == "mN3LO":
+        AV_Coulomb['O1'] = lambda R: -1*VB/adl.norm_3vec(R)*(1 + alpha_s/(4*np.pi)*(2*beta0*jax.numpy.log(Rprime(R))+aa1) + (alpha_s/(4*np.pi))**2*( beta0**2*(4*jax.numpy.log(Rprime(R))**2 + np.pi**2/3) + 2*( beta1+2*beta0*aa1 )*jax.numpy.log(Rprime(R))+aa2 ) ) + (alpha_s/(4*np.pi))**3*( 64*np.pi**2/3*Nc**3*jax.numpy.log(adl.norm_3vec(R)) + aa3 + 64*np.pi**2/3*Nc**3*np.euler_gamma + 512*beta0**3*( jax.numpy.log(Rprime(R))**3 + np.pi**4/4*jax.numpy.log(Rprime(R))+2*zeta3 ) + (640*beta0*beta1 + 192*beta0**2*aa1)*(jax.numpy.log(Rprime(R))**2+np.pi**2/12) + (128*beta2+64*beta1*aa1+24*beta0*aa2)*jax.numpy.log(Rprime(R)) ) -1*CF*Nc*alpha_s**2/(N_coord-1)/(adl.norm_3vec(R)**2)
+else:
+	print("order not supported")	
+	throw(0)
+
 Coulomb_potential = adl.make_pairwise_potential(AV_Coulomb)
 
 # build Coulomb ground-state trial wavefunction
@@ -419,7 +466,8 @@ print('Measuring <H>...')
 
 Ks = []
 #for R in tqdm.tqdm(gfmc_Rs):
-for R in gfmc_Rs:
+for count, R in enumerate(gfmc_Rs):
+    print('Calculating lapacian for step ', count)
     #Ks.append(-1/2*laplacian_f_R(R) / f_R(R) / adl.mp_Mev)
     Ks.append(-1/2*laplacian_f_R(R) / f_R(R) / 1)
 Ks = np.array(Ks)
@@ -489,21 +537,20 @@ print("H=",Hs,"\n\n")
 print("K=",ave_Ks,"\n\n")
 print("V=",ave_Vs,"\n\n")
 
-#with h5py.File('Hammys_'+"N_coord="+str(N_coord)+"_B="+str(VB)+"_nStep="+str(n_step)+"_dtau="+str(dtau_iMev)+'.hdf5', 'w') as f:
-#    dset = f.create_dataset("default", data=Hs)
+tag = "N_coord="+str(N_coord)+"_alpha="+str(alpha)+"_nStep="+str(n_step)+"_dtau="+str(dtau_iMev)+"_OLO="+str(OLO)+"_spoila="+str(spoila)+"_spoilf="+str(spoilf)
 
-with h5py.File(outdir+'Hammys_'+"N_coord="+str(N_coord)+"_alpha="+str(alpha)+"_nStep="+str(n_step)+"_dtau="+str(dtau_iMev)+"_a0="+str(a0)+'.hdf5', 'w') as f:
+with h5py.File(outdir+'Hammys_'+tag+'.h5', 'w') as f:
     dset = f.create_dataset("Hammys", data=Ks+Vs)
 
-with h5py.File(outdir+'Rs_'+"N_coord="+str(N_coord)+"_alpha="+str(alpha)+"_nStep="+str(n_step)+"_dtau="+str(dtau_iMev)+"_a0="+str(a0)+'.hdf5', 'w') as f:
+with h5py.File(outdir+'Rs_'+tag+'.h5', 'w') as f:
     dset = f.create_dataset("Rs", data=gfmc_Rs)
 
 
-with h5py.File(outdir+'Hammys_'+"N_coord="+str(N_coord)+"_alpha="+str(alpha)+"_nStep="+str(n_step)+"_dtau="+str(dtau_iMev)+"_a0="+str(a0)+'.hdf5', 'r') as f:
+with h5py.File(outdir+'Hammys_'+tag+'.h5', 'r') as f:
     data = f['Hammys']
     print(data)
 
-with h5py.File(outdir+'Rs_'+"N_coord="+str(N_coord)+"_alpha="+str(alpha)+"_nStep="+str(n_step)+"_dtau="+str(dtau_iMev)+"_a0="+str(a0)+'.hdf5', 'r') as f:
+with h5py.File(outdir+'Rs_'+tag, 'r') as f:
     data = f['Rs']
     print(data)
 
@@ -524,4 +571,4 @@ elif N_coord == 7:
     ax.set_ylim(-15, -18)
 ax.legend()
 
-plt.savefig(outdir+'Hammys_'+"N_coord="+str(N_coord)+"_alpha="+str(alpha)+"_nStep="+str(n_step)+"_dtau="+str(dtau_iMev)+"_a0="+str(a0)+'.pdf')
+plt.savefig(outdir+'Hammy_gfmc_plot_'+tag+'.pdf')
