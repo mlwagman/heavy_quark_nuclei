b6e9be9fa1771606c0acfde82d35db106707e422
Michael Wagman <mwagman@mac-135687.Zyxel.com>
2022-11-30 08:47:58 -0600

    tau skip for constant fit

--- constant_fit.py
+++ constant_fit.py
@@ -52,181 +52,180 @@ n_walk_full = dset.shape[1]
 if start_fit == -1:
     start_fit = (dset.shape[0] // 10) + 1
 
-for n_tau_skip_exp in range(round(np.log(dset.shape[0]//n_walk_full+1)/np.log(2)), round(np.log(dset.shape[0])/np.log(2))-1):
-    n_tau_skip = 2**n_tau_skip_exp
-    print("\nTRYING N_TAU_SKIP = ", n_tau_skip)
-    #full_data = np.real(dset[start_fit::n_tau_skip] * dset_Ws[start_fit::n_tau_skip])
-    #full_Ws = np.real(dset_Ws[start_fit::n_tau_skip])
+n_tau_skip_exp = round(np.log(dset.shape[0]//n_walk_full+1)/np.log(2))
+n_tau_skip = 2**(n_tau_skip_exp+1)
+print("\nTRYING N_TAU_SKIP = ", n_tau_skip)
+#full_data = np.real(dset[start_fit::n_tau_skip] * dset_Ws[start_fit::n_tau_skip])
+#full_Ws = np.real(dset_Ws[start_fit::n_tau_skip])
 
-    n_step = (dset.shape[0] - start_fit) // n_tau_skip
-    full_data = np.zeros((n_step, n_walk_full))
-    full_Ws = np.zeros((n_step, n_walk_full))
-    for tau in range(n_step):
-        for k in range(n_tau_skip):
-            full_data[tau] += np.real(dset[start_fit+tau*n_tau_skip+k] * dset_Ws[start_fit+tau*n_tau_skip+k])
-            full_Ws[tau] += np.real(dset_Ws[start_fit+tau*n_tau_skip+k])
-    
-    if n_skip > 1:
-        n_walk = n_walk_full // n_skip
-    elif n_block > 1:
-        n_walk = n_walk_full // n_block
-    else :
-        n_walk = n_walk_full
-    
-    # block data
+n_step = (dset.shape[0] - start_fit) // n_tau_skip
+full_data = np.zeros((n_step, n_walk_full))
+full_Ws = np.zeros((n_step, n_walk_full))
+for tau in range(n_step):
+    for k in range(n_tau_skip):
+        full_data[tau] += np.real(dset[start_fit+tau*n_tau_skip+k] * dset_Ws[start_fit+tau*n_tau_skip+k])
+        full_Ws[tau] += np.real(dset_Ws[start_fit+tau*n_tau_skip+k])
+
+if n_skip > 1:
+    n_walk = n_walk_full // n_skip
+elif n_block > 1:
+    n_walk = n_walk_full // n_block
+else :
+    n_walk = n_walk_full
+
+# block data
+data = np.zeros((n_step,n_walk))
+Ws = np.zeros((n_step,n_walk))
+for i in range(n_walk):
+    for k in range(n_block):
+        data[:,i] += full_data[:,i*n_block+k]/n_block
+        Ws[:,i] += full_Ws[:,i*n_block+k]/n_block
+
+# sparsen data
+if n_skip > 1:
     data = np.zeros((n_step,n_walk))
     Ws = np.zeros((n_step,n_walk))
     for i in range(n_walk):
-        for k in range(n_block):
-            data[:,i] += full_data[:,i*n_block+k]/n_block
-            Ws[:,i] += full_Ws[:,i*n_block+k]/n_block
-    
-    # sparsen data
-    if n_skip > 1:
-        data = np.zeros((n_step,n_walk))
-        Ws = np.zeros((n_step,n_walk))
+        data[:,i] += full_data[:,i*n_skip]
+        Ws[:,i] += full_Ws[:,i*n_skip]
+
+# sample mean for each step
+sample_mean = np.zeros((n_step))
+sample_mean_num = np.zeros((n_step))
+for n in range(n_step):
+    sample_mean[n] = np.mean(data[n]) / np.mean(Ws[n])
+    sample_mean_num[n] = np.mean(data[n])
+
+print("\n SAMPLE MEAN")
+print(np.size(sample_mean))
+print(sample_mean[0:n_step:n_print])
+
+# sample covariance
+sample_covar_num = np.zeros((n_step,n_step))
+for n in range(n_step):
+    for m in range(n_step):
+       sample_covar_num[n,m] = (np.mean(data[n]*data[m]) - np.mean(data[n])*np.mean(data[m])) * n_walk/(n_walk-1)
+
+#print("\n SAMPLE ERR")
+#print(sample_covar.shape)
+#print(np.array([np.sqrt(sample_covar[n,n]/n_walk) for n in range(0,n_step,n_print)]))
+
+# bootstrap ensemble means
+boot_ensemble = np.zeros((n_boot, n_step))
+for b in range(n_boot):
+    inds = np.random.randint(n_walk, size=n_walk)
+    for n in range(n_step):
+        this_boot = data[n][inds]
+        this_boot_Ws = Ws[n][inds]
+        boot_ensemble[b,n] = np.mean(this_boot) / np.mean(this_boot_Ws)
+
+#print("\n BOOTSTRAP MEAN")
+#print(boot_ensemble.shape)
+#print(np.array([np.mean(boot_ensemble[:,n]) for n in range(0,n_step,n_print)]))
+
+# bootstrap variance
+boot_var = np.zeros((n_step))
+for n in range(n_step):
+    boot_var[n] = (np.mean(boot_ensemble[:,n]**2) - np.mean(boot_ensemble[:,n])**2) * n_walk/(n_walk-1)
+
+print("\n BOOTSTRAP ERR")
+print(boot_var.shape)
+print(np.sqrt(boot_var[0:n_step:n_print]))
+
+# bootstrap covariance
+boot_covar = np.zeros((n_step,n_step))
+for n in range(n_step):
+    for m in range(n_step):
+       boot_covar[n,m] = (np.mean(boot_ensemble[:,n]*boot_ensemble[:,m]) - np.mean(boot_ensemble[:,n])*np.mean(boot_ensemble[:,m])) * n_walk/(n_walk-1)
+
+#print("\n BOOTSTRAP COVARIANCE")
+#print(boot_covar.shape)
+#print("\n DIAGONAL")
+#print(np.array([boot_covar[n,n] for n in range(0, n_step, n_print)]))
+#print("\n TOP ROW")
+#print(np.array([boot_covar[0,n] for n in range(0, n_step, n_print)]))
+
+# normalized sample mean and covariance
+norm_data = np.array([ (data[n,:] - sample_mean_num[n])/np.sqrt(sample_covar_num[n,n]) for n in range(n_step) ])
+norm_covar = np.array([ [ sample_covar_num[n,m]/np.sqrt(sample_covar_num[n,n]*sample_covar_num[m,m]) for m in range(n_step) ] for n in range(n_step) ])
+
+# determine optimal shrinkage parameter
+d2 = 0.0
+for n in range(n_step):
+    for m in range(n_step):
+        if n == m:
+            d2 += (norm_covar[n,m] - 1)**2
+        else:
+            d2 += norm_covar[n,m]**2
+d2 /= n_step
+b2 = 0.0
+for n in range(n_step):
+    for m in range(n_step):
         for i in range(n_walk):
-            data[:,i] += full_data[:,i*n_skip]
-            Ws[:,i] += full_Ws[:,i*n_skip]
+            b2 += (norm_data[n,i]*norm_data[m,i] - norm_covar[n,m])**2
+b2 /= (n_step*n_walk*n_walk)
+lam = b2/d2
+if lam > 1:
+    lam = 1
+elif lam < 0:
+    lam = 0
+print("\n OPTIMAL SHRINKAGE PARAMETER")
+print(lam)
+
+if lam >= 0.9:
+    delta_fit = 0.0
+    fit = np.mean(sample_mean)
+    delta_fit = np.sqrt((sample_mean - fit) @ (sample_mean - fit))/np.sqrt(n_step)
+    chisq = 0.0 
+    dof = n_step-1
+    chi_red = chisq/dof
+    plot_scale = 1e6
     
-    # sample mean for each step
-    sample_mean = np.zeros((n_step))
-    sample_mean_num = np.zeros((n_step))
-    for n in range(n_step):
-        sample_mean[n] = np.mean(data[n]) / np.mean(Ws[n])
-        sample_mean_num[n] = np.mean(data[n])
-    
-    print("\n SAMPLE MEAN")
-    print(np.size(sample_mean))
-    print(sample_mean[0:n_step:n_print])
-    
-    # sample covariance
-    sample_covar_num = np.zeros((n_step,n_step))
-    for n in range(n_step):
-        for m in range(n_step):
-           sample_covar_num[n,m] = (np.mean(data[n]*data[m]) - np.mean(data[n])*np.mean(data[m])) * n_walk/(n_walk-1)
-    
-    #print("\n SAMPLE ERR")
-    #print(sample_covar.shape)
-    #print(np.array([np.sqrt(sample_covar[n,n]/n_walk) for n in range(0,n_step,n_print)]))
-    
-    # bootstrap ensemble means
-    boot_ensemble = np.zeros((n_boot, n_step))
-    for b in range(n_boot):
-        inds = np.random.randint(n_walk, size=n_walk)
-        for n in range(n_step):
-            this_boot = data[n][inds]
-            this_boot_Ws = Ws[n][inds]
-            boot_ensemble[b,n] = np.mean(this_boot) / np.mean(this_boot_Ws)
-    
-    #print("\n BOOTSTRAP MEAN")
-    #print(boot_ensemble.shape)
-    #print(np.array([np.mean(boot_ensemble[:,n]) for n in range(0,n_step,n_print)]))
-    
-    # bootstrap variance
-    boot_var = np.zeros((n_step))
-    for n in range(n_step):
-        boot_var[n] = (np.mean(boot_ensemble[:,n]**2) - np.mean(boot_ensemble[:,n])**2) * n_walk/(n_walk-1)
-    
-    print("\n BOOTSTRAP ERR")
-    print(boot_var.shape)
-    print(np.sqrt(boot_var[0:n_step:n_print]))
-    
-    # bootstrap covariance
-    boot_covar = np.zeros((n_step,n_step))
-    for n in range(n_step):
-        for m in range(n_step):
-           boot_covar[n,m] = (np.mean(boot_ensemble[:,n]*boot_ensemble[:,m]) - np.mean(boot_ensemble[:,n])*np.mean(boot_ensemble[:,m])) * n_walk/(n_walk-1)
-    
-    #print("\n BOOTSTRAP COVARIANCE")
-    #print(boot_covar.shape)
-    #print("\n DIAGONAL")
-    #print(np.array([boot_covar[n,n] for n in range(0, n_step, n_print)]))
-    #print("\n TOP ROW")
-    #print(np.array([boot_covar[0,n] for n in range(0, n_step, n_print)]))
-    
-    # normalized sample mean and covariance
-    norm_data = np.array([ (data[n,:] - sample_mean_num[n])/np.sqrt(sample_covar_num[n,n]) for n in range(n_step) ])
-    norm_covar = np.array([ [ sample_covar_num[n,m]/np.sqrt(sample_covar_num[n,n]*sample_covar_num[m,m]) for m in range(n_step) ] for n in range(n_step) ])
-    
-    # determine optimal shrinkage parameter
-    d2 = 0.0
+    print("\n FIT RESULT")
+    print("SINGULAR COVARIANCE MATRIX")
+    print("n_start = ", start_fit)
+    print(fit, " +/- ", delta_fit)
+    print("dof = ", dof)
+    print("chi^2/dof = ", chi_red)
+
+else:
+    # apply shrinkage
+    boot_covar_shrunk = np.zeros((n_step,n_step))
     for n in range(n_step):
         for m in range(n_step):
             if n == m:
-                d2 += (norm_covar[n,m] - 1)**2
+                boot_covar_shrunk[n,m] = boot_covar[n,n]
             else:
-                d2 += norm_covar[n,m]**2
-    d2 /= n_step
-    b2 = 0.0
-    for n in range(n_step):
-        for m in range(n_step):
-            for i in range(n_walk):
-                b2 += (norm_data[n,i]*norm_data[m,i] - norm_covar[n,m])**2
-    b2 /= (n_step*n_walk*n_walk)
-    lam = b2/d2
-    if lam > 1:
-        lam = 1
-    elif lam < 0:
-        lam = 0
-    print("\n OPTIMAL SHRINKAGE PARAMETER")
-    print(lam)
+                boot_covar_shrunk[n,m] = (1-lam)*boot_covar[n,m]
     
-    if lam >= 0.9:
-        delta_fit = 0.0
-        fit = np.mean(sample_mean)
-        delta_fit = np.sqrt((sample_mean - fit) @ (sample_mean - fit))/np.sqrt(n_step)
-        chisq = 0.0 
-        dof = n_step-1
-        chi_red = chisq/dof
-        plot_scale = 1e6
-        
-        print("\n FIT RESULT")
-        print("SINGULAR COVARIANCE MATRIX")
-        print("n_start = ", start_fit)
-        print(fit, " +/- ", delta_fit)
-        print("dof = ", dof)
-        print("chi^2/dof = ", chi_red)
-
-        break
-    else:
-        # apply shrinkage
-        boot_covar_shrunk = np.zeros((n_step,n_step))
-        for n in range(n_step):
-            for m in range(n_step):
-                if n == m:
-                    boot_covar_shrunk[n,m] = boot_covar[n,n]
-                else:
-                    boot_covar_shrunk[n,m] = (1-lam)*boot_covar[n,m]
-        
-        print("\n BOOTSTRAP COVARIANCE WITH OPTIMAL SHRINKAGE ERR")
-        print(np.array([np.sqrt(boot_covar_shrunk[n,n]) for n in range(0, n_step, n_print)]))
-        #print("\n TOP ROW")
-        #print(np.array([boot_covar_shrunk[0,n] for n in range(0, n_step, n_print)]))
-        
-        # invert covariance matrix
-        boot_covar_shrunk_inv = np.linalg.inv(boot_covar_shrunk)
-        
-        #print("\n INVERSE BOOTSTRAP COVARIANCE WITH OPTIMAL SHRINKAGE")
-        #print(boot_covar_shrunk_inv.shape)
-        #print("\n DIAGONAL")
-        #print(np.array([boot_covar_shrunk_inv[n,n] for n in range(0, n_step, n_print)]))
-        #print("\n TOP ROW")
-        #print(np.array([boot_covar_shrunk_inv[0,n] for n in range(0, n_step, n_print)]))
-        
-        # do the fit!
-        Delta = 1/np.sum(boot_covar_shrunk_inv, axis=None)
-        delta_fit = np.sqrt(Delta)
-        fit = Delta * np.sum(sample_mean @ boot_covar_shrunk_inv)
-        chisq = (sample_mean - fit) @ boot_covar_shrunk_inv @ (sample_mean - fit)
-        dof = n_step-1
-        chi_red = chisq/dof
-        
-        print("\n FIT RESULT")
-        print("n_start = ", start_fit)
-        print(fit, " +/- ", delta_fit)
-        print("dof = ", dof)
-        print("chi^2/dof = ", chi_red)
+    print("\n BOOTSTRAP COVARIANCE WITH OPTIMAL SHRINKAGE ERR")
+    print(np.array([np.sqrt(boot_covar_shrunk[n,n]) for n in range(0, n_step, n_print)]))
+    #print("\n TOP ROW")
+    #print(np.array([boot_covar_shrunk[0,n] for n in range(0, n_step, n_print)]))
+    
+    # invert covariance matrix
+    boot_covar_shrunk_inv = np.linalg.inv(boot_covar_shrunk)
+    
+    #print("\n INVERSE BOOTSTRAP COVARIANCE WITH OPTIMAL SHRINKAGE")
+    #print(boot_covar_shrunk_inv.shape)
+    #print("\n DIAGONAL")
+    #print(np.array([boot_covar_shrunk_inv[n,n] for n in range(0, n_step, n_print)]))
+    #print("\n TOP ROW")
+    #print(np.array([boot_covar_shrunk_inv[0,n] for n in range(0, n_step, n_print)]))
+    
+    # do the fit!
+    Delta = 1/np.sum(boot_covar_shrunk_inv, axis=None)
+    delta_fit = np.sqrt(Delta)
+    fit = Delta * np.sum(sample_mean @ boot_covar_shrunk_inv)
+    chisq = (sample_mean - fit) @ boot_covar_shrunk_inv @ (sample_mean - fit)
+    dof = n_step-1
+    chi_red = chisq/dof
+    
+    print("\n FIT RESULT")
+    print("n_start = ", start_fit)
+    print(fit, " +/- ", delta_fit)
+    print("dof = ", dof)
+    print("chi^2/dof = ", chi_red)
 
 
 with open(database[:-2]+'csv', 'w', newline='') as csvfile:
