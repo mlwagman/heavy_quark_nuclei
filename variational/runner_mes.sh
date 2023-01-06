c431a5854d1e1685678f0f6c8651531e98e79499
benleo12 <benoit.assi23@gmail.com>
2023-01-06 16:19:39 -0600

    latest variational

--- variational/runner_mes.sh
+++ variational/runner_mes.sh
@@ -4,41 +4,41 @@
 
 #python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=LO --alpha=1 &
 
-python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=1 --OLO=LO --alpha=0.2 &
+#python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=LO --alpha=0.2 &
 
-python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=1 --OLO=LO --alpha=0.6 &
+python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=LO --alpha=0.6 &
 
-python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=1 --OLO=LO --alpha=1 &
+python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=LO --alpha=1 &
 
 
 #python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=NLO --alpha=0.2 &
 
 #python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=NLO --alpha=1 &
 
-python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=1 --OLO=NLO --alpha=0.2 &
+#python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=NLO --alpha=0.2 &
 
-python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=1 --OLO=NLO --alpha=0.6 &
+python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=NLO --alpha=0.6 &
 
-python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=1 --OLO=NLO --alpha=1 &
+python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=NLO --alpha=1 &
 
 
 #python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=mNLO --alpha=0.2 &
 
 #python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=mNLO --alpha=1 &
 
-python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=1 --OLO=mNLO --alpha=0.2 &
+#python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=mNLO --alpha=0.2 &
 
-python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=1 --OLO=mNLO --alpha=0.6 &
+python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=mNLO --alpha=0.6 &
 
-python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=1 --OLO=mNLO --alpha=1 &
+python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=mNLO --alpha=1 &
 
 
 #python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=NNLO --alpha=0.2 &
 
 #python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=NNLO --alpha=1 &
 
-python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=1 --OLO=NNLO --alpha=0.2 &
+#python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=NNLO --alpha=0.2 &
 
-python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=1 --OLO=NNLO --alpha=0.6 &
+python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=NNLO --alpha=0.6 &
 
-python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=1 --OLO=NNLO --alpha=1 &
+python3 heavy_quark_nuclei_variational_Mes.py --log10_learn_rate=3 --c_loss=1  --v_loss=1 --N_exp=3 --OLO=NNLO --alpha=1 &
