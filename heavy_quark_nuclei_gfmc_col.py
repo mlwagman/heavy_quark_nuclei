71b84824bb1d5e37454e8ccae1de44903cedb7f1
benleo12 <benoit.assi23@gmail.com>
2023-04-06 10:53:39 -0500

    new color gfmc

---
+++ heavy_quark_nuclei_gfmc_col.py
@@ -0,0 +1,390 @@
+### Eval script for deuteron GFMC deformation.
+
+import argparse
+import analysis as al
+import getpass
+import matplotlib.pyplot as plt
+import numpy as onp
+import scipy
+import scipy.interpolate
+import scipy.integrate
+import scipy.special
+import pickle
+import paper_plt
+import tqdm.auto as tqdm
+import afdmc_lib as adl
+import os
+import pickle
+from afdmc_lib import NI,NS,mp_Mev,fm_Mev
+import jax
+import jax.numpy as np
+import sys
+from itertools import repeat
+import time
+import h5py
+import math
+import mpmath
+from functools import partial
+
+from itertools import permutations
+import torch
+import torch.nn as nn
+
+onp.random.seed(0)
+
+paper_plt.load_latex_config()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--n_walkers', type=int, default=1000)
+parser.add_argument('--dtau_iMev', type=float, required=True)
+parser.add_argument('--n_step', type=int, required=True)
+parser.add_argument('--n_skip', type=int, default=200)
+parser.add_argument('--resampling', type=int, default=None)
+parser.add_argument('--alpha', type=float, default=1)
+parser.add_argument('--mu', type=float, default=1.0)
+parser.add_argument('--mufac', type=float, default=1.0)
+parser.add_argument('--Nc', type=int, default=3)
+parser.add_argument('--N_coord', type=int, default=2)
+parser.add_argument('--nf', type=int, default=5)
+parser.add_argument('--OLO', type=str, default="LO")
+parser.add_argument('--spoila', type=int, default=1)
+parser.add_argument('--spoilf', type=str, default="hwf")
+parser.add_argument('--outdir', type=str, required=True)
+parser.add_argument('--input_Rs_database', type=str, default="")
+parser.add_argument('--log_mu_r', type=float, default=1)
+parser.add_argument('--cutoff', type=float, default=0.0)
+parser.add_argument('--verbose', dest='verbose', action='store_true', default=False)
+globals().update(vars(parser.parse_args()))
+
+#######################################################################################
+
+CF = (Nc**2 - 1)/(2*Nc)
+VB = alpha*CF
+if N_coord > 2:
+    VB = alpha*CF/(Nc-1)
+SingC3 = -(Nc+1)/8
+a0 = spoila*2/VB;
+#a0=4.514
+
+#VB=.1
+#print(VB)
+#quit()
+# imaginary time points for GFMC evolution
+tau_iMev = dtau_iMev * n_step
+xs = np.linspace(0, tau_iMev, endpoint=True, num=n_step+1)
+
+beta0 = 11/3*Nc - 2/3*nf
+beta1 = 34/3*Nc**2 - 20/3*Nc*nf/2 - 2*CF*nf
+beta2 = 2857/54*Nc**3 + CF**2*nf-205/9*Nc*CF*nf/2-1415/27*Nc**2*nf/2+44/9*CF*(nf/2)**2+158/27*Nc*(nf/2)**2
+aa1 = 31/9*Nc-10/9*nf
+zeta3 = scipy.special.zeta(3)
+zeta5 = scipy.special.zeta(5)
+zeta51 = 1/2 + 1/3 + 1/7 + 1/51 + 1/4284
+zeta6 = scipy.special.zeta(6)
+aa2 = ( 4343/162 + 6*np.pi**2 - np.pi**4/4 + 22/3*zeta3 )*Nc**2 - ( 1798/81 + 56/3*zeta3 )*Nc*nf/2 - ( 55/3 - 16*zeta3  )*CF*nf/2 + (10/9*nf)**2
+dFF = (18-Nc**2+Nc**4)/(96*Nc**2)
+dFA = Nc*(Nc**2+6)/48
+alpha4 = float(mpmath.polylog(4,1/2))*0+(-np.log(2))**4/(4*3*2*1)
+ss6 = zeta51+zeta6
+
+L = log_mu_r
+VB_LO = VB
+
+VB_NLO = VB * (1 + alpha/(4*np.pi)*(aa1 + 2*beta0*L))
+
+if OLO == "LO":
+    a0=spoila*2/VB_LO
+elif OLO == "NLO":
+    a0=spoila*2/VB_NLO
+
+
+Rprime = lambda R: adl.norm_3vec(R)*np.exp(np.euler_gamma)*mu
+# build Coulomb potential
+AV_Coulomb = {}
+if OLO == "LO":
+    @partial(jax.jit)
+    def potential_fun(R):
+	    return -1*VB/adl.norm_3vec(R)
+elif OLO == "NLO":
+    @partial(jax.jit)
+    def potential_fun(R):
+        return -1*VB/adl.norm_3vec(R)*(1 + alpha/(4*np.pi)*(2*beta0*np.log(Rprime(R))+aa1))
+else:
+	print("order not supported")
+	throw(0)
+
+#TODO change potential definition
+#AV_Coulomb['O1'] = potential_fun
+#Coulomb_potential = adl.make_pairwise_potential(AV_Coulomb, B3_Coulomb)
+
+
+
+
+# build Coulomb ground-state trial wavefunction
+@partial(jax.jit)
+def f_R(Rs):
+    #N_walkers = Rs.shape[0]
+    #assert Rs.shape == (N_walkers, N_coord, 3)
+    psi = 1
+    for i in range(N_coord):
+       	for j in range(N_coord):
+            if i!=j and j>=i:
+                ri = Rs[...,i,:]
+                rj = Rs[...,j,:]
+                rij_norm = adl.norm_3vec(ri - rj)
+                psi = psi*np.exp(-rij_norm/a0)
+    return psi
+
+def cutoff_fn(Rs):
+    psi = 1
+    for i in range(N_coord):
+       	for j in range(N_coord):
+            if i!=j and j>=i:
+                ri = Rs[...,i,:]
+                rj = Rs[...,j,:]
+                rij_norm = adl.norm_3vec(ri - rj)
+                psi = psi*np.exp(-cutoff / rij_norm)
+    return psi
+
+
+def cutoff_f_R_sq(Rs):
+    return np.abs( f_R(Rs) )**2 * cutoff_fn(Rs)
+
+@partial(jax.jit)
+def laplacian_f_R(Rs):
+    #N_walkers = Rs.shape[0]
+    #assert Rs.shape == (N_walkers, N_coord, 3)
+    nabla_psi_tot = 0
+    # terms where laplacian hits one piece of wvfn
+    # laplacian hits r_kl
+    for k in range(N_coord):
+        for l in range(N_coord):
+            if k!=l and l>=k:
+                # wvfn includes r_ij
+                nabla_psi = 1
+                for i in range(N_coord):
+                    for j in range(N_coord):
+                        if i!=j and j>=i:
+                            ri = Rs[...,i,:]
+                            rj = Rs[...,j,:]
+                            rij_norm = adl.norm_3vec(ri - rj)
+                            # nabla_k^2 r_kl = nabla_l^2 r_kl
+                            # factor of two included to account for both terms appearing in laplacian
+                            if k == i and l == j:
+                                nabla_psi = nabla_psi * (2/a0**2 - 4/(a0*rij_norm)) * np.exp(-rij_norm/a0)
+                            else:
+                                nabla_psi = nabla_psi * np.exp(-rij_norm/a0)
+                nabla_psi_tot += nabla_psi
+    # terms where gradients hit separate pieces of wvfn
+    # laplacian involves particle a
+    for a in range(N_coord):
+        # first gradient involves r_kl
+        for k in range(N_coord):
+            for l in range(N_coord):
+                if k!=l and l>=k and (a==k or a==l):
+                    # second gradient involves r_mn
+                    for m in range(N_coord):
+                        for n in range(N_coord):
+                            if m!=n and n>=m and (m!=k or n!=l) and (a==m or a==n):
+                                # sum over the 3-d components of gradient
+                                for x in range(3):
+                                    # wvfn involves r_ij
+                                    nabla_psi = 1
+                                    for i in range(N_coord):
+                                        for j in range(N_coord):
+                                            if i!=j and j>=i:
+                                                ri = Rs[...,i,:]
+                                                rj = Rs[...,j,:]
+                                                rij_norm = adl.norm_3vec(ri - rj)
+                                                rsign = 0
+                                                # grad_a r_ij = rsign * (ri - rj)
+                                                if a == i:
+                                                    rsign = 1
+                                                elif a == j:
+                                                    rsign = -1
+                                                if (k == i and l == j) or (m == i and n == j):
+                                                    nabla_psi = rsign * nabla_psi * (ri[:,x] - rj[:,x])/(a0*rij_norm) * np.exp(-rij_norm/a0)
+                                                else:
+                                                    nabla_psi = nabla_psi * np.exp(-rij_norm/a0)
+                                    nabla_psi_tot += nabla_psi
+    return nabla_psi_tot
+
+
+# Metropolis
+if input_Rs_database == "":
+    R0 = onp.random.normal(size=(N_coord,3))
+    # set center of mass position to 0
+    R0 -= onp.mean(R0, axis=1, keepdims=True)
+    #samples = adl.metropolis(R0, f_R, n_therm=500, n_step=n_walkers, n_skip=n_skip, eps=2*a0/N_coord**2)
+    samples = adl.metropolis(R0, cutoff_f_R_sq, n_therm=500, n_step=n_walkers, n_skip=n_skip, eps=2*a0/N_coord**2)
+    Rs_metropolis = np.array([R for R,_ in samples])
+else:
+    f = h5py.File(input_Rs_database, 'r')
+    Rs_metropolis = f["Rs"][-1]
+print(Rs_metropolis)
+# build trial wavefunction
+S_av4p_metropolis = onp.zeros(shape=(Rs_metropolis.shape[0],) + (NI,NS)*N_coord).astype(np.complex128)
+print("built Metropolis wavefunction ensemble")
+# trial wavefunction spin-flavor structure is |up,u> x |up,u> x ... x |up,u>
+
+def levi_civita(i, j, k):
+    if i == j or j == k or i == k:
+        return 0
+    if (i,j,k) in [(0,1,2), (1,2,0), (2,0,1)]:
+        return 1
+    else:
+        return -1
+
+for i in range(N_coord):
+    for j in range(N_coord):
+        for k in range(N_coord):
+            if i != j and j != k and i != k:
+                spin_slice = (slice(0, None),) + (i, j, k) + (0,) * N_coord
+                    S_av4p_metropolis[spin_slice] = levi_civita(i, j, k)
+
+print("spin-flavor wavefunction shape = ", S_av4p_metropolis.shape)
+
+#print("old ", f_R_old(Rs_metropolis))
+#print("new ", f_R(Rs_metropolis))
+
+#print("old laplacian ", laplacian_f_R_old(Rs_metropolis))
+#print("new laplacian ", laplacian_f_R(Rs_metropolis))
+
+# trivial contour deformation
+deform_f = lambda x, params: x
+params = (np.zeros((n_step+1)),)
+
+print('Running GFMC evolution:')
+rand_draws = onp.random.random(size=(n_step, Rs_metropolis.shape[0]))
+gfmc = adl.gfmc_deform(
+    Rs_metropolis, S_av4p_metropolis, f_R, params,
+    rand_draws=rand_draws, tau_iMev=tau_iMev, N=n_step, potential=Coulomb_potential,
+    deform_f=deform_f, m_Mev=adl.mp_Mev,
+    resampling_freq=resampling)
+gfmc_Rs = np.array([Rs for Rs,_,_,_, in gfmc])
+gfmc_Ws = np.array([Ws for _,_,_,Ws, in gfmc]) / cutoff_fn(gfmc_Rs)
+gfmc_Ss = np.array([Ss for _,_,Ss,_, in gfmc])
+
+print('GFMC tau=0 weights:', gfmc_Ws[0])
+print('GFMC tau=dtau weights:', gfmc_Ws[1])
+
+# measure H
+print('Measuring <H>...')
+
+Ks = []
+#for R in tqdm.tqdm(gfmc_Rs):
+for count, R in enumerate(gfmc_Rs):
+    print('Calculating Laplacian for step ', count)
+    K_time = time.time()
+    #Ks.append(-1/2*laplacian_f_R(R) / f_R(R) / adl.mp_Mev)
+    Ks.append(-1/2*laplacian_f_R(R) / f_R(R) / 1)
+    print(f"calculated kinetic in {time.time() - K_time} sec")
+Ks = np.array(Ks)
+
+#Ks *= fm_Mev**2
+
+#Vs = np.array([
+#    sum([
+#        AV_Coulomb[name](dRs) * adl.compute_O(adl.two_body_ops[name](dRs), S, S_av4p_metropolis)
+#        for name in AV_Coulomb
+#    ])
+#    for dRs, S in zip(map(adl.to_relative, gfmc_Rs), gfmc_Ss)])
+
+Vs = []
+for count, R in enumerate(gfmc_Rs):
+    print('Calculating potential for step ', count)
+    V_time = time.time()
+    VSI,_ = Coulomb_potential(R)
+    V_ind = (slice(0,None),) + (0,)*NS*NI*N_coord
+    print(f"calculated potential in {time.time() - V_time} sec")
+    Vs.append(VSI)
+
+Vs = np.array(Vs)
+
+print(Vs.shape)
+
+tag = str(OLO) + "_dtau"+str(dtau_iMev) + "_Nstep"+str(n_step) + "_Nwalkers"+str(n_walkers) + "_Ncoord"+str(N_coord) + "_Nc"+str(Nc) + "_Nf"+str(nf) + "_alpha"+str(alpha) + "_spoila"+str(spoila) + "_spoilf"+str(spoilf) + "_log_mu_r"+str(log_mu_r)
+
+with h5py.File(outdir+'Hammys_'+tag+'.h5', 'w') as f:
+    dset = f.create_dataset("Hammys", data=Ks+Vs)
+    dset = f.create_dataset("Ws", data=gfmc_Ws)
+
+with h5py.File(outdir+'Hammys_'+tag+'.h5', 'r') as f:
+    data = f['Hammys']
+    print(data)
+
+with h5py.File(outdir+'Rs_'+tag+'.h5', 'w') as f:
+    dset = f.create_dataset("Rs", data=gfmc_Rs)
+    dset = f.create_dataset("Ws", data=gfmc_Ws)
+
+with h5py.File(outdir+'Rs_'+tag+'.h5', 'r') as f:
+    data = f['Rs']
+    print(data)
+
+## plot H
+#fig, ax = plt.subplots(1,1, figsize=(4,3))
+#al.add_errorbar(np.transpose(Hs/(VB**2)), ax=ax, xs=xs, color='xkcd:forest green', label=r'$\left< H \right>$', marker='o')
+#if N_coord == 2:
+#    ax.set_ylim(-.26, -.24)
+#elif N_coord == 3:
+#    ax.set_ylim(-1.1, -1.05)
+#elif N_coord == 4:
+#    ax.set_ylim(-2.5, -3.5)
+#elif N_coord == 5:
+#    ax.set_ylim(-5, -6)
+#elif N_coord == 6:
+#    ax.set_ylim(-9, -11)
+#elif N_coord == 7:
+#    ax.set_ylim(-15, -18)
+#ax.legend()
+#
+#plt.savefig(outdir+'Hammy_gfmc_plot_'+tag+'.pdf')
+
+if verbose:
+
+    Hs = np.array([al.bootstrap(K + V, W, Nboot=100, f=adl.rw_mean)
+            for K,V,W in zip(Ks, Vs, gfmc_Ws)])
+
+    ave_Ks = np.array([al.bootstrap(K, W, Nboot=100, f=adl.rw_mean)
+            for K,V,W in zip(Ks, Vs, gfmc_Ws)])
+    ave_Vs = np.array([al.bootstrap(V, W, Nboot=100, f=adl.rw_mean)
+            for K,V,W in zip(Ks, Vs, gfmc_Ws)])
+
+    print("first walker")
+    print(gfmc_Rs.shape)
+    print("R = ",gfmc_Rs[0][0])
+    x = gfmc_Rs[0][:,:,0]
+    y = gfmc_Rs[0][:,:,1]
+    z = gfmc_Rs[0][:,:,2]
+    r_n = np.sqrt(x**2 + y**2 + z**2)
+    t_n = np.arctan2(np.sqrt(x**2 + y**2), z)
+    p_n = np.arctan2(y, x)
+    print("r = ",r_n[0,0])
+    print("theta = ",t_n[0,0])
+    print("phi = ",p_n[0,0])
+    print("psi(R) = ",f_R(gfmc_Rs[0])[0])
+    print("K(R) = ",Ks[0,0])
+    print("V(R) = ",Vs[0,0])
+    print("H(R) = ",Ks[0,0]+Vs[0,0])
+
+    print("\n", Ks.shape)
+
+    print("\nsecond walker")
+    print("R = ",gfmc_Rs[0][1])
+    x = gfmc_Rs[0][:,:,0]
+    y = gfmc_Rs[0][:,:,1]
+    z = gfmc_Rs[0][:,:,2]
+    r_n = np.sqrt(x**2 + y**2 + z**2)
+    t_n = np.arctan2(np.sqrt(x**2 + y**2), z)
+    p_n = np.arctan2(y, x)
+    print("r = ",r_n[0,1])
+    print("theta = ",t_n[0,1])
+    print("phi = ",p_n[0,1])
+    print("psi(R) = ",f_R(gfmc_Rs[0])[1])
+    print("K(R) = ",Ks[0,1])
+    print("V(R) = ",Vs[0,1])
+    print("H(R) = ",Ks[0,1]+Vs[0,1])
+
+    print("H=",Hs,"\n\n")
+    print("K=",ave_Ks,"\n\n")
+    print("V=",ave_Vs,"\n\n")
