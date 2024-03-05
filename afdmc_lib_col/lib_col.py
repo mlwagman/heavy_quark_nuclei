1ff658b4a4447dae9146aaaa5b2ed81661562a1f
Michael Wagman <mwagman@fnal.gov>
2024-03-05 17:24:29 -0600

    more drift

--- afdmc_lib_col/lib_col.py
+++ afdmc_lib_col/lib_col.py
@@ -47,8 +47,8 @@ def step_G0_symm_distinct(R, *, dtau_iMev, m_Mev):
     lam_fm = np.sqrt(2/m_Mev * fm_Mev * dtau_fm)
     (n_walkers, n_coord, n_d) = R.shape
     dR = 1/onp.sqrt(2) * onp.random.normal(size=R.shape)
-    #drift = np.zeros((n_walkers, n_d)) 
-    #drift_vec = onp.zeros((n_walkers, n_coord, n_d)) 
+    #drift = np.zeros((n_walkers, n_d))
+    #drift_vec = onp.zeros((n_walkers, n_coord, n_d))
     for i in range(0, n_coord):
         dR[:,i,:] = dR[:,i,:] * lam_fm[i]
     #    drift += dR[:,i,:] * m_Mev[i] / n_coord
@@ -57,7 +57,7 @@ def step_G0_symm_distinct(R, *, dtau_iMev, m_Mev):
     #    #drift_vec[:,i,:] = drift[:,:] / m_Mev[i]
     #    drift_vec[:,i,:] = drift[:,:] / np.mean(m_Mev)
     ## subtract mean dR to avoid "drift" in the system
-    ##dR -= onp.mean(dR, axis=1, keepdims=True)
+    #dR -= onp.mean(dR, axis=1, keepdims=True)
     #dR -= drift_vec
     return R+dR, R-dR
 
@@ -1048,10 +1048,10 @@ def gfmc_deform(
         R, R_deform, S, W = walkers
 
         (n_walkers, n_coord, n_d) = R.shape
-        drift = np.zeros((n_walkers, n_d)) 
+        drift = np.zeros((n_walkers, n_d))
         for i in range(0, n_coord):
             drift += R[:,i,:] * m_Mev[i] / n_coord / np.mean(m_Mev)
-        print("checking drift = 0 =", np.sum(np.abs(drift)))
+        print("checking drift = 0 =", np.mean(drift, axis=0))
         print("<W^2>/<W>^2 = ", np.mean(W*W)/np.mean(W))
         print("<r_first> = ", np.mean(W*np.transpose(norm_3vec(R)[:,0]))/np.mean(W))
         print("<r_last> = ", np.mean(W*np.transpose(norm_3vec(R)[:,-1]))/np.mean(W))
