7a4744303b1e46dc9b7d356b4e7c720ed5dbc4bf
Michael Wagman <mwagman@fnal.gov>
2024-07-16 17:59:35 -0500

    deut test driver

--- qqqqqq_test.sh
+++ qqqqqq_test.sh
@@ -1,6 +1,6 @@
 # run GFMC and fit wavefunction based on Hyleraas trial state
-nw=500
-nstep=0
+nw=5
+nstep=5
 #nstep=0
 nskip=100
 
@@ -18,6 +18,7 @@ sa=1.00033
 af=6.82843
 g=0.0
 color="1x1"
+wvfn="product"
 python3 heavy_quark_nuclei_deut.py --alpha $alpha --log_mu_r 0.0 --OLO "LO" --n_step $nstep --n_walkers $nw --dtau $dt --Nc 3 --nf 4 --N_coord 6 --outdir "data/" --wavefunction $wvfn --potential "full" --Lcut 5 --L 0 --afac $af --spoila $sa  --n_skip $nskip --masses 1 2 3 1 2 3 --g $g --color $color --verbose 
 
 file="data/Hammys_LO_dtau${dt}_Nstep${nstep}_Nwalkers${nw}_Ncoord6_Nc3_nskip${nskip}_Nf4_alpha${alpha}_spoila${sa}_spoilaket1_spoilfhwf_spoilS1_log_mu_r0.0_wavefunction_${wvfn}_potential_full_afac${af}_masses[1.0, 1.0, 1.0, 1.0, 1.0, 1.0]_color_${color}_g${g}.h5"
