752b5ed6d7638f91f96483b7857e244235410b42
benleo12 <benoit.assi23@gmail.com>
2024-07-22 11:48:07 -0500

    runners and fitters for mtm tests

---
+++ boosted_fitter_4.py
@@ -0,0 +1,69 @@
+import os
+import glob
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import re
+
+# Function to read the CSV files and extract the data
+def read_data(directory):
+    # Updated pattern to match the filenames
+    pattern = os.path.join(directory, "Hammys_LO_dtau0.3_Nstep300_Nwalkers1000_Ncoord4*_L*_afac1_masses*_mtm*.csv")
+    files = glob.glob(pattern)
+
+    print(f"Found {len(files)} files matching the pattern.")
+
+    data = []
+    for file in files:
+        try:
+            # Extract L from the filename using regex
+            match = re.search(r"_L([0-9.]+)_", file)
+            if match:
+                L = float(match.group(1))
+                df = pd.read_csv(file)
+                mean = df['mean'].values[0]
+                err = df['err'].values[0]
+                data.append((L, mean, err))
+                print(f"Successfully read data from {file}: L={L}, mean={mean}, err={err}")
+            else:
+                print(f"Failed to extract L from {file}")
+        except Exception as e:
+            print(f"Failed to read data from {file}: {e}")
+    
+    return data
+
+# Linear function for fitting
+def linear(x, a, b):
+    return a * x + b
+
+# Main function to read data, fit and plot
+def main():
+    directory = 'mtm_data'
+    data = read_data(directory)
+    
+    if not data:
+        print("No data to process. Exiting.")
+        return
+    
+    # Sort data by L for better plotting
+    data.sort(key=lambda x: x[0])
+    
+    Ls, means, errs = zip(*data)
+    
+    # Fit the data to a linear model
+    popt, pcov = curve_fit(linear, Ls, means, sigma=errs)
+    a, b = popt
+    fit_line = linear(np.array(Ls), a, b)
+    
+    # Plot the data with error bars
+    plt.errorbar(Ls, means, yerr=errs, fmt='o', label='Data')
+    plt.plot(Ls, fit_line, label=f'Fit: y = {a:.2e}x + {b:.2e}', color='red')
+    plt.xlabel('L')
+    plt.ylabel('mean')
+    plt.title('L vs mean with linear fit')
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
--- mtm_test.sh
+++ mtm_test.sh
@@ -1,13 +1,23 @@
 # run GFMC and fit wavefunction based on Hyleraas trial state
-nw=2
-nstep=25
+nw=1000
+nstep=300 
 nskip=10
+
+# Function to calculate pi using bc
+calculate_pi() {
+    echo "scale=10; 4*a(1)" | bc -l
+}
+
+# Store the value of pi in a variable
+pi=$(calculate_pi)
+
+# Print the value of pi
+echo "The value of pi is: $pi"
 
 alpha=0.75
 mu=1.0
-dt=0.01
+dt=0.3
 
-#### ttcc ###
 
 sa=1.0
 g=0.0
@@ -19,10 +29,22 @@ m2=1.0
 wvfn="compact"
 sa=1.0
 af=1
-python3 heavy_quark_nuclei_gfmc_boosted_FV.py --alpha $alpha --log_mu_r 0.0 --mu $mu --OLO "LO" --n_step $nstep --n_walkers $nw --dtau $dt --Nc 3 --nf 4 --N_coord 2 --outdir "data/" --wavefunction $wvfn --potential "full" --Lcut 5 --L 0 --spoila $sa  --n_skip $nskip --masses $m1 -$m2 --g $g --color $color  --verbose --mtm_x 1 1
+#L=20
+n_x=1.0
+n_y=0.0
+n_z=0.0
 
-#file="data/Hammys_LO_dtau${dt}_Nstep${nstep}_Nwalkers${nw}_Ncoord2_Nc3_nskip${nskip}_Nf4_alpha${alpha}_spoila${sa}_log_mu_r0.0_wavefunction_${wvfn}_potential_full_afac${af}_masses[$m1, -$m2]_color_${color}_g${g}.h5"
-#python average_constant_fit.py "--database=${file}" --dtau $dt # --n_tau_tol 100
+# Loop over the Lattice size
+for L in 10 20 30 40 50 60 70 80 90 100; do
+    mtm_x_1=$(echo "scale=10; 2 * $pi * $n_x / $L" | bc -l)
+    mtm_x_2=$(echo "scale=10; 2 * $pi * $n_x / $L" | bc -l)
+    mtm_y_1=$(echo "scale=10; 2 * $pi * $n_y / $L" | bc -l)
+    mtm_y_2=$(echo "scale=10; 2 * $pi * $n_y / $L" | bc -l)
+    mtm_z_1=$(echo "scale=10; 2 * $pi * $n_z / $L" | bc -l)
+    mtm_z_2=$(echo "scale=10; 2 * $pi * $n_z / $L" | bc -l)
+
+    #python3 heavy_quark_nuclei_gfmc_boosted_FV.py --alpha $alpha --log_mu_r 0.0 --mu $mu --OLO "LO" --n_step $nstep --n_walkers $nw --dtau $dt --Nc 3 --nf 4 --N_coord 2 --outdir "mtm_data/" --wavefunction $wvfn --potential "full" --Lcut 5 --L $L --spoila $sa --n_skip $nskip --masses $m1 -$m2 --g $g --color $color --mtm_x $mtm_x_1 $mtm_x_2 --mtm_y $mtm_y_1 $mtm_y_2 --mtm_z $mtm_z_1 $mtm_z_2 #--verbose
+done
 
 # molecular spatial wavefunction 
 wvfn="product"
@@ -33,21 +55,30 @@ g=1.0
 gfac=1.0
 
 sa=1.0
-af=1000000000000000.0
+af=1e8
 lmur=0.0
 color="1x1"
 
-python3 heavy_quark_nuclei_gfmc_boosted_FV.py --alpha $alpha --log_mu_r $lmur --mu $mu --OLO "LO" --n_step $nstep --n_walkers $nw --dtau $dt --Nc 3 --nf 4 --N_coord 4 --outdir "data/" --wavefunction $wvfn --potential "full" --Lcut 5 --L 0 --afac $af --spoila $sa  --n_skip $nskip --masses $m1  -$m2  $m1 -$m2 --g $g --color $color --gfac $gfac --verbose --mtm_x 1 1 -1 -1
+# Loop over the Lattice size
+for L in 10 20 30 40 50 60 70 80 90 100; do
+    mtm_x_1=$(echo "scale=10; 2 * $pi * $n_x / $L" | bc -l)
+    mtm_x_2=$(echo "scale=10; 2 * $pi * $n_x / $L" | bc -l)
+    mtm_x_3=$(echo "scale=10; 2 * $pi * $n_x / $L" | bc -l)
+    mtm_x_4=$(echo "scale=10; 2 * $pi * $n_x / $L" | bc -l)
+    mtm_y_1=$(echo "scale=10; 2 * $pi * $n_y / $L" | bc -l)
+    mtm_y_2=$(echo "scale=10; 2 * $pi * $n_y / $L" | bc -l)
+    mtm_y_3=$(echo "scale=10; 2 * $pi * $n_y / $L" | bc -l)
+    mtm_y_4=$(echo "scale=10; 2 * $pi * $n_y / $L" | bc -l)
+    mtm_z_1=$(echo "scale=10; 2 * $pi * $n_z / $L" | bc -l)
+    mtm_z_2=$(echo "scale=10; 2 * $pi * $n_z / $L" | bc -l)
+    mtm_z_3=$(echo "scale=10; 2 * $pi * $n_z / $L" | bc -l)
+    mtm_z_4=$(echo "scale=10; 2 * $pi * $n_z / $L" | bc -l)
 
-exit
+    python3 heavy_quark_nuclei_gfmc_boosted_FV.py --alpha $alpha --log_mu_r $lmur --mu $mu --OLO "LO" --n_step $nstep --n_walkers $nw --dtau $dt --Nc 3 --nf 4 --N_coord 4 --outdir "mtm_data/" --wavefunction $wvfn --potential "full" --Lcut 5 --L 0 --afac $af --spoila $sa  --n_skip $nskip --masses $m1  -$m2  $m1 -$m2 --g $g --color $color --gfac $gfac --mtm_x $mtm_x_1 $mtm_x_2 $mtm_x_3 $mtm_x_4 --mtm_y $mtm_y_1 $mtm_y_2 $mtm_y_3 $mtm_y_4 --mtm_z $mtm_z_1 $mtm_z_2 $mtm_z_3 $mtm_z_4 #--verbose
+done
 
-sa=2.0
-af=2.0
-lmur=0.0
-color="3x3bar"
-
-python3 heavy_quark_nuclei_gfmc_FV.py --alpha $alpha --log_mu_r $lmur --mu $mu --OLO "LO" --n_step $nstep --n_walkers $nw --dtau $dt --Nc 3 --nf 4 --N_coord 4 --outdir "data/" --wavefunction $wvfn --potential "full" --Lcut 5 --L 0 --afac $af --spoila $sa  --n_skip $nskip --masses $m1 $m1  -$m2 -$m2 --g $g --color $color --gfac $gfac --verbose
-
-#file="data/Hammys_LO_dtau${dt}_Nstep${nstep}_Nwalkers${nw}_Ncoord4_Nc3_nskip${nskip}_Nf4_alpha${alpha}_spoila${sa}_log_mu_r0.0_wavefunction_${wvfn}_potential_full_afac${af}_masses[$m1, $m1, -$m2, -$m2]_color_${color}_g${g}.h5"
-#python average_constant_fit.py "--database=${file}" --dtau $dt # --n_tau_tol 100
-
+#don't fit if pdf already exists
+for file in mtm_data/Hammys*mtm*.h5
+    do
+        python3 average_constant_fit.py "--database=${file}" --dtau $dt # --n_tau_tol 100
+done
