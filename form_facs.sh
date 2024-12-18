8d7f04bf85a2ebfb0a40d9e1eeb25f08b1c70e29
Michael Wagman <mlwagman@gmail.com>
2024-12-18 11:13:13 -0600

    q2 range

--- form_facs.sh
+++ form_facs.sh
@@ -39,7 +39,8 @@ n_z=1
 n1_values="(1,0,0)"
 #change the L/change alpha small
 # Loop over the Lattice size
-for Q in 0.0 0.01 0.1 1.0 10.0; do
+#for Q in 0.0 0.01 0.1 1.0 10.0; do
+for Q in 0.0 0.001 0.002 0.003 0.004 0.005 0.006 0.007 0.008 0.009 0.01; do
 
     for n1 in $n1_values; do
         # Parse the n1 tuple into its components
@@ -56,7 +57,7 @@ for Q in 0.0 0.01 0.1 1.0 10.0; do
 
         #python heavy_quark_nuclei_gfmc_boosted_FV.py --Q $Q --alpha $alpha --log_mu_r 0.0 --mu $mu --OLO "LO" --n_step $nstep --n_walkers $nw --dtau $dt --Nc 3 --nf 4 --N_coord 3 --outdir "mtm_data/" --wavefunction $wvfn --potential "full" --Lcut 5 --L 0 --spoila $sa --n_skip $nskip --masses $m1 $m2 $m3 --g $g --color $color --mtm_x $mtm_x_1 --mtm_y $mtm_y_1 --mtm_z $mtm_z_1 --verbose
         #
-        python heavy_quark_nuclei_gfmc_boosted_FV.py --Q $Q --alpha $alpha --log_mu_r 0.0 --mu $mu --OLO "LO" --n_step $nstep --n_walkers $nw --dtau $dt --Nc 3 --nf 4 --N_coord 2 --outdir "mtm_data/" --wavefunction $wvfn --potential "full" --Lcut 5 --L 0 --spoila $sa --n_skip $nskip --masses $m1 -$m2 --g $g --color $color --mtm_x $mtm_x_1 --mtm_y $mtm_y_1 --mtm_z $mtm_z_1 --verbose
+        python3 heavy_quark_nuclei_gfmc_boosted_FV.py --Q $Q --alpha $alpha --log_mu_r 0.0 --mu $mu --OLO "LO" --n_step $nstep --n_walkers $nw --dtau $dt --Nc 3 --nf 4 --N_coord 2 --outdir "mtm_data/" --wavefunction $wvfn --potential "full" --Lcut 5 --L 0 --spoila $sa --n_skip $nskip --masses $m1 -$m2 --g $g --color $color --mtm_x $mtm_x_1 --mtm_y $mtm_y_1 --mtm_z $mtm_z_1 --verbose
     done
 done
 
